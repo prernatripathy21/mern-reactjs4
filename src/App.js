6da1e06633a279a1e1859fe42028057ad5e2cd67
Prerna Tripathy <prernatripathy21@gmail.com>
2023-10-14 22:33:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserTable from './components/UserTable';
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserTable.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function UserTable() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API when the component mounts
+    axios.get('https://dummyjson.com/users')
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UserTable;
